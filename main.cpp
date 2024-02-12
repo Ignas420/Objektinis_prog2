89655a042faa72d554a4f1d29a73dc31e33642b5
Ignas Sarapinas <ignas.sarapinas@mif.stud.vu.lt>
2024-02-12 15:38:34 +0200

    added mediana

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <algorithm>
 
 using namespace std;
 const int CMax = 10;
@@ -16,7 +17,7 @@ void Vidurkis(Mokinys A[], int n, int m);
 
 int main(){
     int n, m;
-    Mokinys A[CMax];
+    Mokinys A[CMax], B[CMax];
     cout << "Mokiniu skaicius: ";
     cin >> m;
     cout<<endl;
@@ -24,23 +25,35 @@ int main(){
     cin >> n;
     cout << endl;
     cout << "Iveskite varda, pavarde, pazymius uz namu darbus bei egzamino rezultata: ";
-
     for(int i=0; i<m; i++){
         cin >> A[i].vardas >> A[i].pavarde;
         for(int j=0; j<n; j++){
             cin >> A[i].ND[j];
+            B[i].ND[j]=A[i].ND[j];
         }
         cin >> A[i].egzaminas;
+        B[i].ND[n] = A[i].egzaminas; 
     }
-
     Vidurkis(A, n, m);
-
     cout<<endl;
     cout << "Pavardė" << setfill(' ') << setw(10) << "Vardas" << setfill(' ') << setw(35) << "Galutinis (Vid.) / Galutinis(Med.)"<<endl;
     cout << "------------------------------------------"<<endl;
+
+    double MED[CMax];
+
+    for(int i=0; i<m; i++){
+        sort(B[i].ND, B[i].ND + (n+1));
+        for(int j= 0; j<=n; j++)
+            cout << B[i].ND[j] << " ";
+        cout<<endl;
+        if((n+1)%2==0)
+            MED[i] = (double) (B[i].ND[(n+1)/2 - 1] + B[i].ND[(n+1)/2]) / 2.0;
+        else
+            MED[i] = B[i].ND[(n+1)/2];
+    }
     
     for(int i = 0; i<m; i++){
-        cout << A[i].vardas << setw(10) << A[i].pavarde << setw(10) << " " << endl;
+        cout << A[i].vardas << setw(10) << A[i].pavarde << setw(10) << fixed << setprecision(2) << VID[i] << " " << MED[i] << endl;
     }
     return 0;
 }
